e480d76299b161eba91f21068bf80c7a3f56b2e9
Antonio Balbino S. dos Santos <81731727+abalsantana@users.noreply.github.com>
2021-09-17 10:23:39 -0300

    Update hanoi.c

--- Recursividade/hanoi.c
+++ Recursividade/hanoi.c
@@ -6,16 +6,17 @@
 void hanoi(int n, char origem, char destino, char aux){
 	if(n>0){
 		hanoi(n-1,origem,aux,destino);
-		printf("%c >> %c\n", origem, destino);
-		hanoi(n-1,origem,destino,aux);
+		printf("Mova o disco %d da pilha %c para a pilha %c\n", n, origem, destino);
+		hanoi(n-1,aux,destino,origem);
 	}
 }
 
 int main(void) {
 	int n;
-	char origem,destino,aux;
-	printf("Informe o valor de n:");
+	char origem = 'A', destino = 'C', aux = 'B';
+	printf("Informe o numro de discos:");
 	scanf("%d",&n);
-	hanoi(n)
+	hanoi(n,origem,destino,aux);
 	return 0;
 }
+
